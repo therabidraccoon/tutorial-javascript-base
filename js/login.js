6615d7acc4171eff2fdb2c893c2ca45e7a1100ba
Ivan Maltese <ivan.maltese@objectmethod.it>
2021-05-10 14:38:26 +0200

    updated per promise

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+const TOKEN = "auth-token";
+
 function login() {
   let options = {
     method: "POST",
@@ -20,8 +22,44 @@ function login() {
     data.text().then((token) => {
       console.log("TOKEN ARRIVATO:");
       console.log(token);
+      //   sessionStorage.setItem(TOKEN, token);
+      localStorage.setItem(TOKEN, token);
     });
   });
 
   console.log("FINE LOGIN");
+}
+
+function printToken() {
+  //   let token = sessionStorage.getItem(TOKEN);
+  let token = localStorage.getItem(TOKEN);
+  alert(token);
+}
+
+function test() {
+  let options = {
+    method: "GET",
+    headers: {
+      "auth-token": localStorage.getItem(TOKEN),
+    },
+  };
+
+  console.log("INIZIO TEST");
+  let userName = null;
+  let testPromise = fetch("http://localhost:8080/test", options);
+
+  testPromise.then(function (data) {
+    console.log("RISPOSTA ARRIVATA");
+    console.log(data);
+    data.json().then((user) => {
+      console.log("TEST ARRIVATO:");
+      console.log(user);
+      userName = user["username"];
+      document.getElementById("welcome").style = "display: block";
+      document.getElementById("dati-utente").innerHTML =
+        "NomeUtente: " + userName;
+    });
+  });
+
+  console.log("FINE TEST");
 }
